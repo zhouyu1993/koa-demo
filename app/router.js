d8a47f77da864c7f1513d4f77220560c438481f0
zhouyu1993 <1490079545@qq.com>
2019-02-18 17:55:24 +0800

    update

--- app/router.js
+++ app/router.js
@@ -37,6 +37,157 @@ router
       }
     })
   })
+  .get('/test/json2html', async (ctx, next) => {
+    const data = {
+      "name": "点名册正面",
+      "data": [
+        [
+          "课程名称：快乐童声4"
+        ],
+        [
+          "温馨提示：                                                                   "
+        ],
+        [
+          null,
+          "1.起始课提前10分钟进教室，衔接课提前5分钟进教室、不早退、不拖堂；",
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          "教师姓名：陈继芸"
+        ],
+        [
+          null,
+          "2.着专业规定服装；",
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          "课程时段：周二19:00-20:30"
+        ],
+        [
+          null,
+          "3.日常教务请使用呼叫器。                                                            ",
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          "授课教室：2B"
+        ],
+        [
+          null,
+          "所在中心：万达金街     电话：86368275/86368295         ",
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          "课间休息时间:(5分钟)"
+        ],
+        [
+          "序号",
+          "小名",
+          "学员姓名",
+          "性别",
+          "出生年月",
+          1,
+          2,
+          3,
+          4,
+          5,
+          6,
+          7,
+          8,
+          9,
+          10,
+          11,
+          12,
+          13,
+          14,
+          "已报学科及备注"
+        ],
+        [
+          null,
+          null,
+          null,
+          null,
+          null,
+          "月   日",
+          "月   日",
+          "月   日",
+          "月   日",
+          "月   日",
+          "月   日",
+          "月   日",
+          "月   日",
+          "月   日",
+          "月   日",
+          "月   日",
+          "月   日",
+          "月   日",
+          "月   日"
+        ],
+        [
+          1,
+          null,
+          "徐国祯",
+          "男",
+          "2014/8/21"
+        ]
+      ]
+    }
+
+    const length = Math.max(...data.data.map(item => item.length))
+
+    data.data = data.data.map(item => {
+      const arr = Array.from(new Array(length - item.length)).map(value => null)
+
+      return item.concat(arr)
+    })
+
+    await ctx.render('index', {
+      data: {
+        title: 'test/json2html',
+        feature: './feature/test/json2html/index',
+        data: data
+      }
+    })
+  })
 
   .get('/api/items', async (ctx, next) => {
     console.log('debug', ctx.request.query)
